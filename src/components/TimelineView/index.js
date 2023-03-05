3e49ceaad880c70feb08ae77f363b5b0b6e66578
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-03-05 11:46:26 +0530

    Update index.js

--- src/components/TimelineView/index.js
+++ src/components/TimelineView/index.js
@@ -17,32 +17,38 @@ class TimelineView extends Component {
     }
   }
 
+  renderItem = () => {
+    const {timelineItemsList} = this.props
+
+    timelineItemsList.map(eachItem => {
+      if (eachItem.categoryId === 'COURSE') {
+        return <CourseTimelineCard courseDetails={eachItem} key={eachItem.id} />
+      }
+      return <ProjectTimelineCard projectDetails={eachItem} key={eachItem.id} />
+    })
+  }
+
   render() {
     const {timelineItemsList} = this.props
 
     return (
-      <div className="chrono-container">
+      <>
         <h1 className="main-heading">
           MY JOURNEY OF <br />
           <span className="sub-heading">CCBP 4.0</span>
         </h1>
-
-        {/* {timelineItemsList.map(eachItem => this.renderTimelineCard(eachItem))} */}
-        {/* {timelineItemsList.map(eachItem => {
-            if (eachItem.categoryId === 'COURSE') {
-              return this.renderCourseCard()
-            }
-            return this.renderProjectCard()
-          })} */}
-
-        <Chrono
-          theme={{
-            secondary: 'white',
-          }}
-          items={timelineItemsList}
-          mode="VERTICAL_ALTERNATING"
-        />
-      </div>
+        <div className="chrono-container">
+          <Chrono
+            theme={{
+              secondary: 'white',
+            }}
+            items={timelineItemsList}
+            mode="VERTICAL_ALTERNATING"
+          >
+            {this.renderItem()}
+          </Chrono>
+        </div>
+      </>
     )
   }
 }
